a5eff477967a755cb5a592c59de846d34f38ed23
Russell-Gustuv <79741284+Drunk-Giraffe@users.noreply.github.com>
2024-03-03 23:19:03 +0800

    实现内存池与测试

---
+++ buffer_manager/buffer.go
@@ -0,0 +1,80 @@
+package buffer_manager
+
+import (
+	fmgr "file_manager"
+	lmgr "log_manager"
+)
+
+
+type Buffer struct {
+	fm *fmgr.FileManager
+	lm *lmgr.LogManager
+	contents *fmgr.Page
+	blk *fmgr.BlockID
+	pins uint32		//锁定次数
+	txnum int32		//交易号
+	lsn  uint64		//日志序列号
+}
+
+func NewBuffer(file_mgr *fmgr.FileManager, log_mgr *lmgr.LogManager) *Buffer {
+	
+	return &Buffer{
+		fm: file_mgr,
+		lm: log_mgr,
+		txnum: -1,
+		lsn: 0,
+		contents: fmgr.NewPageBySize(file_mgr.BlockSize()),
+	}
+}
+
+func (b *Buffer) Contents() *fmgr.Page {
+	return b.contents
+}
+
+func (b *Buffer) Block() *fmgr.BlockID {
+	return b.blk
+}
+
+func (b *Buffer) SetModified(txnum int32, lsn uint64) {
+	//如果上层组件修改了缓存数据，必须调用这个接口进行标记
+	b.txnum = txnum
+	if lsn>0 {
+		b.lsn = lsn
+	}
+}
+
+func (b *Buffer) IsPinned() bool {
+	//返回当前缓存是否被锁定
+	return b.pins > 0
+}
+
+func (b *Buffer) ModifyingTx() int32 {
+	//返回当前修改缓存的交易号
+	return b.txnum
+}
+
+func (b *Buffer) AssignToBlock(blk *fmgr.BlockID) {
+	//将缓存分配给指定的块
+	b.Flush() //确保当前缓存中的数据被写入磁盘
+	b.blk = blk
+	b.fm.Read(b.blk, b.Contents())
+	b.pins = 0
+}
+
+func (b *Buffer) Flush() {
+	if b.txnum > 0 {
+		b.lm.FlushByLSN(b.lsn)
+		b.fm.Write(b.blk, b.contents)
+		b.txnum = -1
+	}
+}
+
+func (b *Buffer) Pin() {
+	//锁定缓存
+	b.pins++
+}
+
+func (b *Buffer) Unpin() {
+	//解锁缓存
+	b.pins--
+}
---
+++ buffer_manager/buffer_manager.go
@@ -0,0 +1,129 @@
+package buffer_manager
+
+import (
+	"errors"
+	fmgr "file_manager"
+	lmgr "log_manager"
+	"sync"
+	"time"
+)
+
+const (
+	MAX_TIME_WAIT = 3 //分配缓存最大等待时间
+)
+
+type BufferManager struct {
+	buffer_pool      []*Buffer
+	buffer_available uint32
+	mu               sync.Mutex
+}
+
+func NewBufferManager(fm *fmgr.FileManager, lm *lmgr.LogManager, num_buffers uint32) *BufferManager {
+	bm := &BufferManager{
+		buffer_available: num_buffers, //缓存池大小
+	}
+	for i := uint32(0); i < bm.buffer_available; i++ {
+		buffer := NewBuffer(fm, lm)
+		bm.buffer_pool = append(bm.buffer_pool, buffer)
+	}
+	return bm
+}
+
+func (bm *BufferManager) Available() uint32 {
+	bm.mu.Lock()
+	defer bm.mu.Unlock()
+	return bm.buffer_available
+}
+
+func (bm *BufferManager) FlushAll(txnum int32) {
+	//将给定的交易号的数据修改写入磁盘
+	bm.mu.Lock()
+	defer bm.mu.Unlock()
+
+	for _, buffer := range bm.buffer_pool {
+		if buffer.ModifyingTx() == txnum {
+			buffer.Flush()
+		}
+	}
+}
+
+func (bm *BufferManager) Pin(blk *fmgr.BlockID) (*Buffer, error) {
+	//将指定的块分配给缓存
+	bm.mu.Lock()
+	defer bm.mu.Unlock()
+
+	start_time := time.Now()
+	buffer := bm.tryPin(blk)
+	for buffer == nil && !bm.waitTooLong(start_time) {
+		time.Sleep(MAX_TIME_WAIT * time.Second)
+		buffer = bm.tryPin(blk)
+		if buffer == nil {
+			return nil, errors.New("no buffer available, be careful for deadlock")
+		}
+	}
+
+	return buffer, nil
+}
+
+//目前unpin不会触发异步写回磁盘，只是释放缓存
+func (bm *BufferManager) Unpin(buffer *Buffer) {
+	//释放缓存
+	bm.mu.Lock()
+	defer bm.mu.Unlock()
+	if buffer == nil {
+		return
+	}
+	buffer.Unpin()
+	if !buffer.IsPinned() {
+		bm.buffer_available++
+		//notifyAll() //唤醒等待的线程
+	}
+}
+
+
+func (bm *BufferManager) waitTooLong(start_time time.Time) bool {
+	//判断是否等待时间过长
+	return time.Since(start_time) >= MAX_TIME_WAIT*time.Second
+}
+
+func (bm *BufferManager) tryPin(blk *fmgr.BlockID) *Buffer {
+	//判断给定block是否已经在缓存中
+	buffer := bm.findExistingBuffer(blk)
+	if buffer == nil {
+		buffer = bm.chooseUnpinnedBuffer()
+		if buffer == nil {
+			return nil
+		}
+		buffer.AssignToBlock(blk)
+	}
+
+	if !buffer.IsPinned() {
+		bm.buffer_available--
+	}
+
+	buffer.Pin()
+	return buffer
+}
+
+func (bm *BufferManager) findExistingBuffer(blk *fmgr.BlockID) *Buffer {
+	//判断给定block是否已经在缓存中
+	for _, buffer := range bm.buffer_pool {
+		block := buffer.Block()
+		if  block != nil && block.Equal(blk){
+			return buffer
+		}
+	}
+	return nil
+}
+
+func (bm *BufferManager) chooseUnpinnedBuffer() *Buffer {
+	//选择一个未锁定的缓存
+	for _, buffer := range bm.buffer_pool {
+		if !buffer.IsPinned() {
+			return buffer
+		}
+	}
+	return nil
+}
+
+
